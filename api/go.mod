1467bacb69feb2d7fee40a682afda43639fcc66c
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2025-07-10 00:04:34 -0300

    init go backend

---
+++ api/go.mod
@@ -0,0 +1,5 @@
+module celltomata.com
+
+go 1.24.5
+
+require github.com/go-chi/chi/v5 v5.2.2
---
+++ api/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	router := chi.NewRouter()
+
+	router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
+		_, err := writer.Write([]byte("Celltomata is running..."))
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	err := http.ListenAndServe(":3000", router)
+	if err == nil {
+		log.Println("listening on localhost:3000/")
+	} else {
+		log.Println(err)
+	}
+}
