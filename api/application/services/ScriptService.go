a325ed6ddc1d0ae430d6f0a109b05a2bf980e384
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2025-07-13 07:44:37 -0300

    init of ServiceScript

--- api/application/services/ScriptService.go
+++ api/application/services/ScriptService.go
@@ -1,7 +1,15 @@
 package services
 
+import "github.com/trevisharp/celltomata/domain/models"
+
 type ScriptService struct{}
 
-func (s *ScriptService) Generate(script string) *WorldRule {
+func (s *ScriptService) Generate(script string) *models.WorldRule {
+	var commands []any
 
+	commands = append(commands, "oi")
+
+	return &models.WorldRule{
+		Commands: commands,
+	}
 }
